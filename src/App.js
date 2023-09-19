67d46133b93309fac4c26f2396a81225a4f034f3
Jontykamble <88649711+Jontykamble@users.noreply.github.com>
2023-09-19 14:12:31 +0530

    Adding my frontend of Zikool

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './screeen/Home';
+import {BrowserRouter as Router,Routes,Route,Link}
+from "react-router-dom"
+import Loginnn from './screeen/Login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import '../node_modules/bootstrap/dist/js/bootstrap.esm'
+import Signup from './screeen/Signup';
+import { CardProvider } from './components/ContextReducer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CardProvider>
+      
+    
+      <Router>
+
+
+<div>
+  <Routes>
+    <Route path="/home" element={<Home/>}/>
+   <Route path="/" element={<Loginnn/>}/>
+   <Route path="/createuser" element={<Signup/>}/>
+  
+   
+
+  </Routes>
+</div>
+
+
+
+</Router>
+    </CardProvider>
+    
+  )
+  
 }
 
 export default App;
